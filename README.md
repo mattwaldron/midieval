03d9e4be46e7271bf2b762b953afcee5ea192496
matt <waldron.matthew@gmail.com>
2019-12-31 16:51:27 -0600

    fixed hi-Z on avr_rx causing message 01 00 00 all the time

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# midieval
+# midieval: An Arduino(?) based MIDI synthesizer
+
+# Waveform capture
+Looking into MIDI cable plugged into keyboard, pin 4 in ground, pin 5 is signal (1, 2, 3 are NC):
+
+(page 2 from MIDI spec)
+
+        2
+    5       4
+3               1
+
--- mojo/midi_reader/source/mojo_top.v
+++ mojo/midi_reader/source/mojo_top.v
@@ -18,11 +18,16 @@ module mojo_top(
     input avr_tx, // AVR Tx => FPGA Rx
     output avr_rx, // AVR Rx => FPGA Tx
     input avr_rx_busy, // AVR Rx buffer full
-    input midi_signal
+    input midi_signal,
+    output midi_clk_out
   );
  
   wire rst = ~rst_n; // make reset active high
-   
+  reg new_tx_data;
+  reg midi_ready_dly;
+  reg midi_ready;
+  wire [7:0] data;
+  
   avr_interface avr_interface (
     .clk(clk),
     .rst(rst),
@@ -38,8 +43,8 @@ module mojo_top(
     .new_sample(),
     .sample(),
     .sample_channel(),
-    .tx_data(midi_byte_from_detector),
-    .new_tx_data(midi_byte_ready),
+    .tx_data(data),
+    .new_tx_data(new_tx_data),
     .tx_busy(tx_busy),
     .tx_block(avr_rx_busy),
     .rx_data(rx_data),
@@ -48,19 +53,27 @@ module mojo_top(
  
 // these signals should be high-z when not used
 assign spi_miso = 1'bz;
-assign avr_rx = 1'bz;
+//assign avr_rx = 1'bz;
 assign spi_channel = 4'bzzzz;
+assign midi_clk_out = avr_rx;
 
 midi_byte_detect midi_detect (
     .clk(clk), // 50MHz clock
     .rst(rst),
     .midi_signal(midi_signal),
-    .midi_byte_out(midi_byte_from_detector),
+    .midi_byte_out(data),
     .byte_ready(midi_byte_ready)
 );
 
-always @(posedge midi_byte_ready) begin
-  led <= led + 1;
+always @(posedge clk) begin
+  midi_ready_dly <= midi_ready && ~tx_busy;
+  midi_ready <= midi_byte_ready;
+  if (midi_ready & ~midi_ready_dly) begin
+    new_tx_data <= 1;
+    led <= led + 1;
+  end else begin
+    new_tx_data <= 0;
+  end
 end
 
 endmodule
